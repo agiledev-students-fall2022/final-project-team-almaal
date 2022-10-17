6aa978f465be2089df682d5a62d86e26e63e784b
Maisha Mahrin <maisha.mahrin30@gmail.com>
2022-10-17 14:23:59 -0400

    Create a basic home page with React and Ant Desig user story #34

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import './App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer'
--- front-end/src/routes/Home.jsx
+++ front-end/src/routes/Home.jsx
@@ -1,9 +1,84 @@
 import React from 'react'
+import { UserOutlined } from '@ant-design/icons';
+import { Avatar } from 'antd';
+import {Tabs } from 'antd';
+import { useState } from 'react';
+import { useRef } from 'react';
+import { Link } from 'react-router-dom'
+import './Home.css'
+
+
 
 const Home = () => {
-    return (
-        <div>Home</div>
-    )
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  const nameArray = ['l', 'o', 'b', 'o', 'd', 'a', 'n']
+  const jobArray = [
+    'w',
+    'e',
+    'b',
+    ' ',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+  ]
+
+
+  const [mode, setMode] = useState('top');
+  const handleModeChange = (e) => {
+    setMode(e.target.value);
+  };
+
+  return (
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+            <Avatar size={{
+                        xs: 24,
+                        sm: 32,
+                        md: 40,
+                        lg: 64,
+                        xl: 80,
+                        xxl: 100,
+                    }} 
+                icon={<UserOutlined />} />
+                <span className={letterClass}>  Maisha</span>
+                <span className={letterClass}>     Total Investment Up</span>
+                <span className={letterClass}>    Total Investment Down</span>
+        <div>
+            <Tabs
+            defaultActiveKey="1"
+            tabPosition={mode}
+            style={{
+            height: 220,
+            }}
+            items={new Array(30).fill(null).map((_, i) => {
+            const id = String(i);
+            return {
+                label: `Investment-${id}`,
+                key: id,
+                disabled: i === 28,
+                children: `Content of tab ${id}`,
+            };
+        })}
+      />
+        </div>
+          <h2>Front End Developer / Painter </h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Home
+
