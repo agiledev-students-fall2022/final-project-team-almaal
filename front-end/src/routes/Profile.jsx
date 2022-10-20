bf438f6ea3d09b908d6c5fde8302e240504b2614
Zequan Wu <Zequan Wu>
2022-10-20 03:23:57 -0400

    added profile pic username password tabs

--- front-end/src/routes/Profile.jsx
+++ front-end/src/routes/Profile.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Typography, Space, Divider, Avatar, Input, Switch } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
 
-const Profile = () => {
-    return (
-        <div>Profile</div>
-    )
-}
+const { Tab } = Input;
+const { ProfilePic } = Avatar;
+
+const onChange = (checked: boolean) => {
+    console.log(`switch to ${checked}`);
+  };
+
+const Profile = () => (
+        <div>
+            <Space direction="vertical">
+                <Avatar icon={<UserOutlined />}/>
+                <Input placeholder="Enter Username" prefix={<UserOutlined />} />
+                <Input placeholder="Enter Password" prefix={<UserOutlined />} />
+                <Space>Investment Visibility<Switch defaultChecked ></Switch></Space>
+                <Space>Hide Profile<Switch></Switch></Space>
+                <Divider/>
+            </Space>
+        </div>
+);
 
 export default Profile
