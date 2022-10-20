9e76fe7fefc7bca639d3606acd3527e8f761ccaa
Zequan Wu <Zequan Wu>
2022-10-20 15:43:54 -0400

    added tabs for investment info

--- front-end/src/routes/Profile.jsx
+++ front-end/src/routes/Profile.jsx
@@ -1,25 +1,47 @@
-import React from 'react'
-import { Typography, Space, Divider, Avatar, Input, Switch } from 'antd';
-import { UserOutlined } from '@ant-design/icons';
+import React, {useState} from 'react'
+import { Typography, Space, Divider, Avatar,Button, Input, Switch, Card, List, Tooltip } from 'antd';
+import { EditOutlined, UserOutlined } from '@ant-design/icons';
 
-const { Tab } = Input;
+const { Tab } = Input
 const { ProfilePic } = Avatar;
 
-const onChange = (checked: boolean) => {
-    console.log(`switch to ${checked}`);
-  };
-
-const Profile = () => (
+const Profile = ({total_friends=0, total_groups=0, total_investment=0, total_pl=0}) => {
+        const [username, setUsername] = useState('');
+        return(
         <div>
             <Space direction="vertical">
                 <Avatar icon={<UserOutlined />}/>
-                <Input placeholder="Enter Username" prefix={<UserOutlined />} />
-                <Input placeholder="Enter Password" prefix={<UserOutlined />} />
+                Username
+                <Space>
+                    <Input.Group compact>
+                        <Input defaultValue="current_username" disabled />
+                        <Tooltip>
+                            <Button icon={<EditOutlined />} />
+                        </Tooltip>
+                    </Input.Group>
+                </Space>
+                Password
+                <Space>
+                    <Input.Group compact>
+                        <Input defaultValue="current_password" disabled />
+                        <Tooltip>
+                            <Button icon={<EditOutlined />} />
+                        </Tooltip>
+                    </Input.Group>
+                </Space>
                 <Space>Investment Visibility<Switch defaultChecked ></Switch></Space>
                 <Space>Hide Profile<Switch></Switch></Space>
                 <Divider/>
+                <List>
+                    <Space>
+                        <Card size="small" title="Total Friends">{total_friends}</Card>
+                        <Card size="small" title="Total Groups">{total_groups}</Card>
+                        <Card size="small" title="Total Investment">{total_investment}</Card>
+                        <Card size="small" title="Total P/L">{total_pl}</Card>
+                    </Space>
+                </List>
             </Space>
         </div>
 );
-
+}
 export default Profile
