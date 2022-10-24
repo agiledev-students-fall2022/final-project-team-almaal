b02391f3a090cf5d95aa8adc1d1f8f0cde99c952
Maisha Mahrin <maisha.mahrin30@gmail.com>
2022-10-23 23:42:05 -0400

    Created Footer with CSS

--- front-end/src/components/Footer.jsx
+++ front-end/src/components/Footer.jsx
@@ -1,76 +1,23 @@
 import React from 'react'
-import './Footer.css'
-
+import styles from './Footer.module.css'
+import {Link} from "react-router-dom";
 
   
 const Footer = () => {
   return (
-    <footer className="footer">
-    <p>This is react sticky footer!!</p>
-  
+    <footer className={styles.footer}>
+        {/* <div>
+          <Link to='/profile'>Profile</Link>
+        </div>  */}
+        <div>
+           <a className={styles.container} href = "mailto: mm10294@nyu.edu" color="white"> Contact Us </a>      {/*<Link to='/profile'>Profile</Link>      */}
+        </div>
+        <p>
+          &copy;2022. Team Almaal. All rights reserved.
+        </p>
+   
    </footer>
-    // <Box>
-    //   <h1 style={{ color: "green", 
-    //                textAlign: "center", 
-    //                marginTop: "-50px" }}>
-    //     GeeksforGeeks: A Computer Science Portal for Geeks
-    //   </h1>
-    //   <Container>
-    //     <Row>
-    //       <Column>
-    //         <Heading>About Us</Heading>
-    //         <FooterLink href="#">Aim</FooterLink>
-    //         <FooterLink href="#">Vision</FooterLink>
-    //         <FooterLink href="#">Testimonials</FooterLink>
-    //       </Column>
-    //       <Column>
-    //         <Heading>Services</Heading>
-    //         <FooterLink href="#">Writing</FooterLink>
-    //         <FooterLink href="#">Internships</FooterLink>
-    //         <FooterLink href="#">Coding</FooterLink>
-    //         <FooterLink href="#">Teaching</FooterLink>
-    //       </Column>
-    //       <Column>
-    //         <Heading>Contact Us</Heading>
-    //         <FooterLink href="#">Uttar Pradesh</FooterLink>
-    //         <FooterLink href="#">Ahemdabad</FooterLink>
-    //         <FooterLink href="#">Indore</FooterLink>
-    //         <FooterLink href="#">Mumbai</FooterLink>
-    //       </Column>
-    //       <Column>
-    //         <Heading>Social Media</Heading>
-    //         <FooterLink href="#">
-    //           <i className="fab fa-facebook-f">
-    //             <span style={{ marginLeft: "10px" }}>
-    //               Facebook
-    //             </span>
-    //           </i>
-    //         </FooterLink>
-    //         <FooterLink href="#">
-    //           <i className="fab fa-instagram">
-    //             <span style={{ marginLeft: "10px" }}>
-    //               Instagram
-    //             </span>
-    //           </i>
-    //         </FooterLink>
-    //         <FooterLink href="#">
-    //           <i className="fab fa-twitter">
-    //             <span style={{ marginLeft: "10px" }}>
-    //               Twitter
-    //             </span>
-    //           </i>
-    //         </FooterLink>
-    //         <FooterLink href="#">
-    //           <i className="fab fa-youtube">
-    //             <span style={{ marginLeft: "10px" }}>
-    //               Youtube
-    //             </span>
-    //           </i>
-    //         </FooterLink>
-    //       </Column>
-    //     </Row>
-    //   </Container>
-    // </Box>
+
   );
-};
+}
 export default Footer;
