02a59724d0ebfa1cc939d9c0409ff019c5acabb4
Zequan Wu <Zequan Wu>
2022-11-07 02:45:06 -0500

    logout option and POST route to update profile info

--- back-end/routes/profile.js
+++ back-end/routes/profile.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 
 // define the profile page route
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     const response = {
         username: "@frankwu2002",
         password: "********",
@@ -15,6 +15,30 @@ router.get('/', (req, res) => {
         total_proflie: "+10.5%"
     }
     res.json(response)
+})
+
+// logout route
+router.get('/logout',async (req,res) => {
+    res.redirect('/login')
+})
+
+// update profile information
+router.post('/update', async (req,res) => {
+    const response = {}
+
+    if(req.body.username){
+        response.username = req.body.username
+    }
+    if(req.body.password){
+        response.password = req.body.username
+    }
+    if(req.body.investment_visibility){
+        response.investment_visibility = req.body.investment_visibility
+    }
+    if(req.body.hide_profile){
+        response.hide_profile = req.body.hide_profile
+    }
+    res.status(200).json(response)
 })
 
 module.exports = router
