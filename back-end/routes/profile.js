d8645b3084ce6cda18b8753aee33d7aad9338ac3
Zequan Wu <Zequan Wu>
2022-11-07 02:23:47 -0500

    Create GET route to display profile page from user story #20 task #101

--- back-end/routes/profile.js
+++ back-end/routes/profile.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const router = express.Router()
-// define the home page route
+
+
+// define the profile page route
 router.get('/', (req, res) => {
-    res.send('Profile page')
+    const response = {
+        username: "@frankwu2002",
+        password: "********",
+        investment_visibility: true,
+        hide_profile: false,
+        total_friends: 10,
+        total_groups: 7,
+        total_invested: "$5000",
+        total_proflie: "+10.5%"
+    }
+    res.json(response)
 })
 
 module.exports = router
