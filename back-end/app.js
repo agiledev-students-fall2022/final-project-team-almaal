739d956943bb2a12570d1d5533ec974a16446847
Wenbo-Bao <wb2128@nyu.edu>
2022-11-08 09:09:53 -0500

    integrate login front end and backend

--- back-end/app.js
+++ back-end/app.js
@@ -6,15 +6,24 @@ const friends = require('./routes/friends')
 const groups = require('./routes/groups')
 const news = require('./routes/news')
 const profile = require('./routes/profile')
+const login = require('./routes/login')
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+//allow cross-origin resource sharing
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
+    next();
+});
 
 app.use('/home', home);
 app.use('/groups', groups);
 app.use('/news', news);
 app.use('/friends', friends);
 app.use('/profile', profile);
+app.use('/login', login);
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
---
+++ back-end/routes/login.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+
+var users = [
+    {
+        email: 'abc@gmail.com',
+        password: 'password'
+    }
+]
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.send("welcome to backend server")
+});
+
+router.post('/', function (req, res) {
+    let result = users.find(user => user.email == req.body.email);
+    if (result) {
+        if (result.password == req.body.password) {
+            res.status(200).send({
+                message: "successful login!!!"
+            })
+        } else {
+            res.status(200).send({
+                message: "Password incorrect!!!"
+            })
+        }
+    } else {
+        res.status(200).send({
+            message: "User not found"
+        })
+    }
+
+})
+
+module.exports = router;
