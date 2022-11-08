c81e13f26156c4730d2f43e21bde70b634726608
Safal Shrestha <safal.bp2@gmail.com>
2022-11-08 01:14:32 -0500

    adding unstaged files in server

--- back-end/app.js
+++ back-end/app.js
@@ -10,6 +10,12 @@ const profile = require('./routes/profile')
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use('/home', home);
 app.use('/groups', groups);
 app.use('/news', news);
