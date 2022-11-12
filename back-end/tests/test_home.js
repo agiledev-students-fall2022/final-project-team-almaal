814a82aee4174305ca8f7c269705593cfedc2b1f
Maisha Mahrin <maisha.mahrin30@gmail.com>
2022-11-12 16:58:27 -0500

    updated tests for home

--- back-end/tests/test_home.js
+++ back-end/tests/test_home.js
@@ -13,7 +13,7 @@ const app = require("../app")
 chai.use(chaiHttp)
 
 describe('API /home', () => {
-    it('it should return 200 and get all the data from database to portfoliomonitor', (done) => {
+    it('GET/ it should return 200 and get all the data from database to portfoliomonitor', (done) => {
         chai.request(app)
             .get('/home/portfolioData')
             .end((err, res) => {
@@ -21,4 +21,25 @@ describe('API /home', () => {
                 done();
             });
     });
+
+    it('GET/ it should return 200 and get all the data from database to portfoliomonitor graph', (done) => {
+        chai.request(app)
+            .get('/home/portfolioChartData')
+            .end((err, res) => {
+                expect(res).to.have.status(200);
+                done();
+            });
+    });
+
+    it('POST/ it should return 200 and send updated json object of investment to backend', (done) => {
+        const investmentData = { key:'1', ticker: 'Tesla', position: 'SELL', quantity:20, price: 103 } // mock investment data
+        chai.request(app)
+            .post('/home/')
+            .send(investmentData)
+            .end((err, res) => {
+                expect(res).to.have.status(200);
+                done();
+            });
+    });
+
 });
