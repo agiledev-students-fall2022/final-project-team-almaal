bba614f837ff3099e70cefe1a872d6e8d439d403
Wenbo-Bao <wb2128@nyu.edu>
2022-12-02 13:16:25 -0500

    start integrating register page

--- jwt/routes/api/auth.js
+++ jwt/routes/api/auth.js
@@ -6,83 +6,82 @@ const jwt = require('jsonwebtoken');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const config = require('config');
-const {check, validationResult} = require('express-validator');
+const { check, validationResult } = require('express-validator');
 
 const { JsonWebTokenError } = require('jsonwebtoken');
-
-
-
 
 // @route   GET api/auth
 // @desc    Test route
 // @access  Public
 router.get('/', auth, async (req, res) => {
-    try{
+    try {
         const user = await User.findById(req.user.id).select('-password');
         res.json(user);
-    } catch(err){
+    } catch (err) {
         console.error(err.message);
         res.status(500).send('Server error');
     }
 });
-
-
 
 // @route   POST api/auth
 // @desc    authenticate user & get token
 // @access  Public
 
-router.post('/', 
-[
-    check('email', 'Please include a valid email')
-    .isEmail(),
-    check('password', 'Password is required')
-    .exists()
-],
-async (req, res) => {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json({errors: errors.array() })
-    }
-
-    const {email, password} = req.body;
-    try{
-        let user = await User.findOne({email});
-        
-        if(!user){
-            return res.status(400).json({errors: [{msg: 'Invalid credentials'}]});
+router.post(
+    '/',
+    [
+        check('email', 'Please include a valid email').isEmail(),
+        check('password', 'Password is required').exists(),
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
         }
 
-        // make sure the password is correct
+        const { email, password } = req.body;
+        try {
+            let user = await User.findOne({ email });
 
-        const isMatch = await bcrypt.compare(password, user.password);
-
-        if(!isMatch){
-            return res.status(400).json({errors: [{msg: 'Invalid credentials'}]});
-        }
-
-        const payload = {
-            user:{
-                id: user.id
+            if (!user) {
+                return res
+                    .status(400)
+                    .json({ errors: [{ msg: 'Invalid credentials' }] });
             }
-        }
 
-        jwt.sign(payload, 
-            config.get('jwtSecret'),
-            {expiresIn: 360000},
-            (err, token) => {
-                if(err) throw err;
-                res.json({token});
+            // make sure the password is correct
+
+            const isMatch = await bcrypt.compare(password, user.password);
+
+            if (!isMatch) {
+                return res
+                    .status(400)
+                    .json({ errors: [{ msg: 'Invalid credentials' }] });
             }
-        );
-        // res.send('user registered');
-        // return jsonwebtoken
-        console.log(req.body);
-    } catch(err){
-        console.error(err.message);
-        res.status(500).send('Server error');
-    }
 
-});
+            const payload = {
+                user: {
+                    id: user.id,
+                },
+            };
+
+            jwt.sign(
+                payload,
+                config.get('jwtSecret'),
+                { expiresIn: 360000 },
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({ token });
+                }
+            );
+            // res.send('user registered');
+            // return jsonwebtoken
+            console.log(req.body);
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server error');
+        }
+    }
+);
 
 module.exports = router;
