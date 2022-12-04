62aeba1417de2b62f49d9f445e726fc3e108d651
Safal Shrestha <safal.bp2@gmail.com>
2022-12-04 15:32:58 -0500

    fixed cors issue with authentication

--- back-end/app.js
+++ back-end/app.js
@@ -33,15 +33,22 @@ app.use(express.json());
 
 //allow cross-origin resource sharing
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept'
-    );
-    res.header(
-        'Access-Control-Allow-Methods',
-        'PUT, POST, GET, DELETE, OPTIONS'
-    );
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, x-auth-token");
+
+
+    // res.header('Access-Control-Allow-Origin', '*');
+    // res.header("Access-Control-Allow-Credentials", "true");
+    // res.header(
+    //     'Access-Control-Allow-Headers',
+    //     'Origin, X-Requested-With, Content-Type, Accept'
+    // );
+    // res.header(
+    //     'Access-Control-Allow-Methods',
+    //     'PUT, POST, GET, DELETE, OPTIONS'
+    // );
     next();
 });
 
