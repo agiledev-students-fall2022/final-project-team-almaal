28534147f5dd2ddadefc6a8545ef376788785a7f
Zequan Wu <Zequan Wu>
2022-12-08 02:14:28 -0500

    can't find bug

--- back-end/routes/profile.js
+++ back-end/routes/profile.js
@@ -8,14 +8,14 @@ const auth = require('../middleware/auth')
 // define the profile page route
 router.get('/', auth, async (req, res) => {
     // console.log("got here"+req.body.id)
-    // try {
-    //     const user = await User.findById("637820a5a5376540710ee44f");
-    // } catch (err) {
-    //     console.error(err.message);
-    //     res.status(500).send('Server error');
-    // }
-    // return res.status(200).json(user)
-    return res.status(200)
+    try {
+        const user = await User.findById("637820a5a5376540710ee44f");
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+    return res.status(200).json(user)
+    // return res.status(200)
     // User.findById(req.body.id, (err,found) => {
     //     if(err){
     //         throw err
